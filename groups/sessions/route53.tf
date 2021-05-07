f1bf44c3f55bf60f94ae51b7b903654d1d23093c
Marty Fox <martin.fox@kainos.com>
2021-05-07 08:40:07 +0100

    Updating live RDS spec to agreed size and storage

--- groups/sessions/route53.tf
+++ groups/sessions/route53.tf
@@ -1,7 +1,7 @@
-resource "aws_route53_record" "sessions_rds" {
-  zone_id = data.aws_route53_zone.private_zone.zone_id
-  name    = "${var.name}db"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [module.sessions_rds.this_db_instance_address]
-}
+# resource "aws_route53_record" "sessions_rds" {
+#   zone_id = data.aws_route53_zone.private_zone.zone_id
+#   name    = "${var.name}db"
+#   type    = "CNAME"
+#   ttl     = "300"
+#   records = [module.sessions_rds.this_db_instance_address]
+# }
