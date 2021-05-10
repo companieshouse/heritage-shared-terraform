aea3cb1b89cda12df30c40276c692e0a4b087ad7
Marty Fox <martin.fox@kainos.com>
2021-05-10 08:55:51 +0100

    Removing commented code no longer required

--- groups/heritage-shared-infrastructure/outputs.tf
+++ groups/heritage-shared-infrastructure/outputs.tf
@@ -1,5 +1,4 @@
 output "rds_addresses" {
-  # value = { for db, settings in var.rds_databases : db => aws_route53_record.rds[db].fqdn }
   value = { for dns in aws_route53_record.rds :
     dns.name => dns.fqdn
   }
