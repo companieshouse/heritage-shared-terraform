0c56aefcb5a891cdc9ce6cf7ac7f92d02ddc09e4
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2021-06-29 09:37:20 +0100

    Added TF vars for CHD BEP

--- groups/heritage-shared-infrastructure/variables.tf
+++ groups/heritage-shared-infrastructure/variables.tf
@@ -62,3 +62,80 @@ variable "vault_password" {
   type        = string
   description = "Password for connecting to Vault - usually supplied through TF_VARS"
 }
+
+# ------------------------------------------------------------------------------
+# CHD BEP Variables
+# ------------------------------------------------------------------------------
+variable "application" {
+  type        = string
+  description = "The name of the application"
+}
+
+variable "nfs_server" {
+  type        = string
+  description = "The name or IP of the environment specific NFS server"
+  default     = null
+}
+
+variable "nfs_mount_destination_parent_dir" {
+  type        = string
+  description = "The parent folder that all NFS shares should be mounted inside on the EC2 instance"
+  default     = "/mnt"
+}
+
+variable "nfs_mounts" {
+  type        = map(any)
+  description = "A map of objects which contains mount details for each mount path required."
+  default = {
+    SH_NFSTest = {                  # The name of the NFS Share from the NFS Server
+      local_mount_point = "folder", # The name of the local folder to mount to if the share name is not wanted
+      mount_options = [             # Traditional mount options as documented for any NFS Share mounts
+        "rw",
+        "wsize=8192"
+      ]
+    }
+  }
+}
+
+variable "bep_default_log_group_retention_in_days" {
+  type        = number
+  default     = 14
+  description = "Total days to retain logs in CloudWatch log group if not specified for specific logs"
+}
+
+variable "bep_app_release_version" {
+  type        = string
+  description = "Version of the application to download for deployment to backend server(s)"
+}
+
+variable "bep_ami_name" {
+  type        = string
+  default     = "ceu-backend-*"
+  description = "Name of the AMI to use in the Auto Scaling configuration for backend server(s)"
+}
+
+variable "bep_instance_size" {
+  type        = string
+  description = "The size of the ec2 instances to build"
+}
+
+variable "bep_min_size" {
+  type        = number
+  description = "The min size of the ASG"
+}
+
+variable "bep_max_size" {
+  type        = number
+  description = "The max size of the ASG"
+}
+
+variable "bep_desired_capacity" {
+  type        = number
+  description = "The desired capacity of ASG"
+}
+
+variable "bep_cw_logs" {
+  type        = map(any)
+  description = "Map of log file information; used to create log groups, IAM permissions and passed to the application to configure remote logging"
+  default     = {}
+}
