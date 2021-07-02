7c90560289fbf6288f61be18af499ad9b0fff6c1
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2021-07-02 11:34:26 +0100

    Added locals and vars to support BEP

--- groups/heritage-shared-infrastructure/locals.tf
+++ groups/heritage-shared-infrastructure/locals.tf
@@ -197,4 +197,36 @@ locals {
     Region    = var.aws_region
     Account   = var.aws_account
   }
+
+# ------------------------------------------------------------------------------
+# CHD BEP
+# ------------------------------------------------------------------------------
+#  chd_bep_data = data.vault_generic_secret.chd_bep_data.data_json
+  chd_ec2_data = data.vault_generic_secret.chd_ec2_data.data
+  s3_releases  = data.vault_generic_secret.s3_releases.data
+
+  kms_keys_data          = data.vault_generic_secret.kms_keys.data
+  security_kms_keys_data = data.vault_generic_secret.security_kms_keys.data
+  logs_kms_key_id        = local.kms_keys_data["logs"]
+  ssm_kms_key_id         = local.security_kms_keys_data["session-manager-kms-key-arn"]
+
+  security_s3_data            = data.vault_generic_secret.security_s3_buckets.data
+  session_manager_bucket_name = local.security_s3_data["session-manager-bucket-name"]
+
+  bep_cw_logs = { for log, map in var.bep_cw_logs : log => merge(map, { "log_group_name" = "${var.application}-bep-${log}" }) }
+  bep_log_groups = compact([for log, map in local.bep_cw_logs : lookup(map, "log_group_name", "")])
+
+  chd_bep_ansible_inputs = {
+    s3_bucket_releases         = local.s3_releases["release_bucket_name"]
+    s3_bucket_configs          = local.s3_releases["config_bucket_name"]
+    heritage_environment       = var.environment
+    version                    = var.bep_app_release_version
+    default_nfs_server_address = var.nfs_server
+    mounts_parent_dir          = var.nfs_mount_destination_parent_dir
+    mounts                     = var.nfs_mounts
+    region                     = var.aws_region
+    cw_log_files               = local.bep_cw_logs
+    cw_agent_user              = "root"
+  }
+
 }
