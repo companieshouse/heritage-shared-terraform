1be378345023209810400bbb790aea744bb5dec3
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2023-02-17 09:42:18 +0000

    Remove deprecated local, construct SG data local based on dynamic lookup

--- groups/sessions/locals.tf
+++ groups/sessions/locals.tf
@@ -2,13 +2,22 @@
 # Locals
 # ------------------------------------------------------------------------
 locals {
-  admin_cidrs   = values(data.vault_generic_secret.internal_cidrs.data)
   sess_rds_data = data.vault_generic_secret.sess_rds.data
   accountIds    = data.vault_generic_secret.account_ids.data
 
   internal_fqdn = format("%s.%s.aws.internal", split("-", var.aws_account)[1], split("-", var.aws_account)[0])
 
   ceu_fe_subnet_cidrs = jsondecode(data.vault_generic_secret.ceu_fe_outputs.data["ceu-frontend-web-subnets-cidrs"])
+
+  rds_ingress_from_services = flatten([
+    for sg_data in data.aws_security_group.rds_ingress : {
+      from_port                = 1521
+      to_port                  = 1521
+      protocol                 = "tcp"
+      description              = "Access from ${sg_data.tags.Name}"
+      source_security_group_id = sg_data.id
+    }
+  ])
 
   default_tags = {
     Terraform = "true"
