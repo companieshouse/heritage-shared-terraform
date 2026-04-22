020cc7496bb15b8345cef62243dd23c40ab2bd5e
Dylan Sides <dsides@companieshouse.gov.uk>
2026-04-22 11:26:44 +0100

    rds_cloudwatch_alarms version upgrade

--- groups/sessions/rds.tf
+++ groups/sessions/rds.tf
@@ -218,7 +218,7 @@ module "rds_start_stop_schedule" {
 }
 
 module "rds_cloudwatch_alarms" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/oracledb_cloudwatch_alarms?ref=tags/1.0.195"
+  source = "git@github.com:companieshouse/terraform-modules//aws/oracledb_cloudwatch_alarms?ref=tags/1.0.236"
 
   db_instance_id        = module.sessions_rds.db_instance_identifier
   db_instance_shortname = upper(var.name)
